5bab22b338a2e5b0b65f17c4896cbf6f01392dec
Peter Kassenaar <info@kassenaar.com>
2020-09-17 09:32:47 +0200

    Added vue.config.js to example Vue Library

---
+++ 200-vue-project/vue.config.js
@@ -0,0 +1,9 @@
+// If you *don't* want your CSS to be extracted to a separate file,
+// uncomment the lines below. Thus, only including the library in the
+// consuming app should be enough.
+
+// module.exports= {
+// 	css: {
+// 		extract: false
+// 	}
+// }
